523398c9ed4758d035c09827b43115285e3b0fd6
Imesh Gunathilaka <imesh562@gmail.com>
2024-06-25 19:39:04 +0530

    Initial changes.

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,9 +1,6 @@
-import 'enums.dart';
-
 class AppConstants {
   static const String appName = 'Resume Radar';
   static bool isPushServiceInitialized = false;
   static const int APP_SESSION_TIMEOUT = 10 * 60;
   static bool IS_USER_LOGGED = false;
-  static ThemeType selectedTheme = ThemeType.LIGHT;
 }
